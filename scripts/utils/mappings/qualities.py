de4da0b4ff77b8a5b2cc63df2d6b21312f98954d
Johman10 <johveck@gmail.com>
2026-03-04 15:58:28 +0200

    fix: remux quality id matching

--- scripts/utils/mappings/qualities.py
+++ scripts/utils/mappings/qualities.py
@@ -4,209 +4,224 @@ QUALITIES = [
         "name": "Raw-HD",
         "description": "Uncompressed, high definition recorded video from airing",
         "radarr": True,
-        "sonarr": True,
+        "sonarr": True
     },
     {
         "id": 2,
         "name": "BR-Disk",
         "description": "Complete Blu-ray disc image",
         "radarr": True,
-        "sonarr": False,
+        "sonarr": False
     },
     {
         "id": 3,
-        "name": "Bluray-2160p Remux",
-        "description": "4K Ultra HD Blu-ray disc content remuxed into a playable file format",
+        "name": "Remux-2160p",
+        "description":
+            "4K Ultra HD Blu-ray disc content remuxed into a playable file format",
         "radarr": True,
-        "sonarr": True,
+        "sonarr": True
     },
     {
         "id": 4,
         "name": "Bluray-2160p",
         "description": "4K Ultra HD Blu-ray video encoded with lossy compression",
         "radarr": True,
-        "sonarr": True,
+        "sonarr": True
     },
     {
         "id": 5,
         "name": "WEBDL-2160p",
-        "description": "4K web download, untouched as released by the streaming service",
+        "description":
+            "4K web download, untouched as released by the streaming service",
         "radarr": True,
-        "sonarr": True,
+        "sonarr": True
     },
     {
         "id": 6,
         "name": "WEBRip-2160p",
-        "description": "4K web rip, either captured from a 4K WEB-DL using a capture card or re-encoded from a 4K WEB-DL",
+        "description":
+            "4K web rip, either captured from a 4K WEB-DL using a capture card or re-encoded from a 4K WEB-DL",
         "radarr": True,
-        "sonarr": True,
+        "sonarr": True
     },
     {
         "id": 7,
         "name": "HDTV-2160p",
         "description": "4K high-definition digital television capture",
         "radarr": True,
-        "sonarr": True,
+        "sonarr": True
     },
     {
         "id": 8,
-        "name": "Bluray-1080p Remux",
-        "description": "1080p Blu-ray disc content remuxed into a playable file format",
+        "name": "Remux-1080p",
+        "description":
+            "1080p Blu-ray disc content remuxed into a playable file format",
         "radarr": True,
-        "sonarr": True,
+        "sonarr": True
     },
     {
         "id": 9,
         "name": "WEBDL-1080p",
-        "description": "1080p web download, untouched as released by the streaming service",
+        "description":
+            "1080p web download, untouched as released by the streaming service",
         "radarr": True,
-        "sonarr": True,
+        "sonarr": True
     },
     {
         "id": 10,
         "name": "Bluray-1080p",
         "description": "1080p Blu-ray video encoded with lossy compression",
         "radarr": True,
-        "sonarr": True,
+        "sonarr": True
     },
     {
         "id": 11,
         "name": "WEBRip-1080p",
-        "description": "1080p web rip, either captured using a capture card or re-encoded from a WEB-DL of equal or higher resolution",
+        "description":
+            "1080p web rip, either captured using a capture card or re-encoded from a WEB-DL of equal or higher resolution",
         "radarr": True,
-        "sonarr": True,
+        "sonarr": True
     },
     {
         "id": 12,
         "name": "HDTV-1080p",
         "description": "1080p high-definition digital television capture",
         "radarr": True,
-        "sonarr": True,
+        "sonarr": True
     },
     {
         "id": 13,
         "name": "Bluray-720p",
         "description": "720p Blu-ray video encoded with lossy compression",
         "radarr": True,
-        "sonarr": True,
+        "sonarr": True
     },
     {
         "id": 14,
         "name": "WEBDL-720p",
-        "description": "720p web download, untouched as released by the streaming service",
+        "description":
+            "720p web download, untouched as released by the streaming service",
         "radarr": True,
-        "sonarr": True,
+        "sonarr": True
     },
     {
         "id": 15,
         "name": "WEBRip-720p",
-        "description": "720p web rip, either captured using a capture card or re-encoded from a WEB-DL of equal or higher resolution",
+        "description":
+            "720p web rip, either captured using a capture card or re-encoded from a WEB-DL of equal or higher resolution",
         "radarr": True,
-        "sonarr": True,
+        "sonarr": True
     },
     {
         "id": 16,
         "name": "HDTV-720p",
         "description": "720p high-definition digital television capture",
         "radarr": True,
-        "sonarr": True,
+        "sonarr": True
     },
     {
         "id": 17,
         "name": "Bluray-576p",
         "description": "576p Blu-ray video encoded with lossy compression",
         "radarr": True,
-        "sonarr": True,
+        "sonarr": True
     },
     {
         "id": 18,
         "name": "Bluray-480p",
         "description": "480p Blu-ray video encoded with lossy compression",
         "radarr": True,
-        "sonarr": True,
+        "sonarr": True
     },
     {
         "id": 19,
         "name": "WEBDL-480p",
-        "description": "480p web download, untouched as released by the streaming service",
+        "description":
+            "480p web download, untouched as released by the streaming service",
         "radarr": True,
-        "sonarr": True,
+        "sonarr": True
     },
     {
         "id": 20,
         "name": "WEBRip-480p",
-        "description": "480p web rip, either captured using a capture card or re-encoded from a WEB-DL of equal or higher resolution",
+        "description":
+            "480p web rip, either captured using a capture card or re-encoded from a WEB-DL of equal or higher resolution",
         "radarr": True,
-        "sonarr": True,
+        "sonarr": True
     },
     {
         "id": 21,
         "name": "DVD-R",
         "description": "DVD-Video disc image",
         "radarr": True,
-        "sonarr": False,
+        "sonarr": False
     },
     {
         "id": 22,
         "name": "DVD",
         "description": "Standard DVD video, usually encoded at 480p",
         "radarr": True,
-        "sonarr": True,
+        "sonarr": True
     },
     {
         "id": 23,
         "name": "DVDSCR",
         "description": "DVD screener, usually a lower quality early release",
         "radarr": True,
-        "sonarr": False,
+        "sonarr": False
     },
     {
         "id": 24,
         "name": "SDTV",
-        "description": "Standard-definition digital television capture, typically 480p or lower",
+        "description":
+            "Standard-definition digital television capture, typically 480p or lower",
         "radarr": True,
-        "sonarr": True,
+        "sonarr": True
     },
     {
         "id": 25,
         "name": "Telecine",
-        "description": "Movie captured from a film print using a telecine machine",
+        "description":
+            "Movie captured from a film print using a telecine machine",
         "radarr": True,
-        "sonarr": False,
+        "sonarr": False
     },
     {
         "id": 26,
         "name": "Telesync",
-        "description": "Filmed in a movie theater using a professional camera, often with external audio",
+        "description":
+            "Filmed in a movie theater using a professional camera, often with external audio",
         "radarr": True,
-        "sonarr": False,
+        "sonarr": False
     },
     {
         "id": 27,
         "name": "REGIONAL",
         "description": "A release intended for a specific geographic region",
         "radarr": True,
-        "sonarr": False,
+        "sonarr": False
     },
     {
         "id": 28,
         "name": "WORKPRINT",
-        "description": "An unfinished version of a movie, often with incomplete special effects",
+        "description":
+            "An unfinished version of a movie, often with incomplete special effects",
         "radarr": True,
-        "sonarr": False,
+        "sonarr": False
     },
     {
         "id": 29,
         "name": "CAM",
-        "description": "Filmed in a movie theater using a camcorder or mobile phone",
+        "description":
+            "Filmed in a movie theater using a camcorder or mobile phone",
         "radarr": True,
-        "sonarr": False,
+        "sonarr": False
     },
     {
         "id": 30,
         "name": "Unknown",
         "description": "Quality or source is unknown or unspecified",
         "radarr": True,
-        "sonarr": True,
-    },
+        "sonarr": True
+    }
 ]
